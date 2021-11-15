140bf26e46851f5f116e0cc6c26b9428ed60bc02
Michel Max <mich.max95@gmail.com>
2021-11-15 15:36:36 +0100

    Corrected errors in call and return ops.

--- include/Processor.h
+++ include/Processor.h
@@ -5,10 +5,13 @@
 #ifndef CHIP8_PROCESSOR_H
 #define CHIP8_PROCESSOR_H
 
+#include <cassert>
 #include "Memory.h"
 #include "Display.h"
 #include "CodeLoader.h"
 #include "RegisterFile.h"
+
+#include <boost/format.hpp>
 
 #define LOWER_MASK 0xF0
 #define HIGHER_MASK 0x0F
@@ -30,53 +33,89 @@ public:
         this->registerFile.SP = 0; //stack starts at address 0x200, yet SP is 0 if it points to 0x200
     }
 
+    void runProgam() {
+        while (this->registerFile.PC >= 0 && this-> registerFile.PC < this->instr->size) {
+            executeInstruction();
+        }
+        std::cout << "Regular Termination." << std::endl;
+    }
+
     void executeInstruction() {
-        switch (this->instr[registerFile.PC] & LOWER_MASK) {
+        std::cout << "Instruction: " << boost::format("%02x") % +this->instr->code[registerFile.PC] << boost::format("%02x") % +this->instr->code[registerFile.PC + 1] << " at " << this->registerFile.PC << std::dec << std::endl;
+        switch (this->instr->code[registerFile.PC] & LOWER_MASK) {
             case 0x00:
                 handle0instr();
                 break;
             case 0x10:
                 handle1instr();
                 break;
+            case 0x20:
+                handle2instr();
+                break;
+            default:
+                std::cerr << "Unknown instruction: " << boost::format("%02x") % +this->instr->code[registerFile.PC] << boost::format("%02x") % +this->instr->code[registerFile.PC + 1] << std::endl;
+                break;
         }
     }
 
+    void dumpRegisters() {
+        std::cout << "Registers:" << std::endl;
+        for (int i = 0; i < 16; i++) {
+            std::cout << "V" << i << ": " << +this->registerFile.V[i] << std::endl;
+        }
+        std::cout << "PC: " << +this->registerFile.PC <<std::endl;
+        std::cout << "SP: " << +this->registerFile.SP <<std::endl;
+        std::cout << "DT: " << +this->registerFile.DT <<std::endl;
+        std::cout << "I: " << +this->registerFile.I <<std::endl;
+        std::cout << "ST: " << +this->registerFile.ST <<std::endl;
+        std::cout << "End of Registers." << std::endl;
+    }
+
+private:
+
     void handle0instr() {
-        if (this->instr[this->registerFile.PC] == 0) {
-            if (this->instr[this->registerFile.PC + 1] == 0xE0) {
+        if (this->instr->code[this->registerFile.PC] == 0) {
+            if (this->instr->code[this->registerFile.PC + 1] == 0xE0) {
                 //CLS
                 this->display->clear();
                 this->registerFile.PC += 2;
                 return;
             }
-            if (this->instr[this->registerFile.PC + 1] == 0xEE) {
+            if (this->instr->code[this->registerFile.PC + 1] == 0xEE) {
                 //RET
-                static_assert(registerFile.SP < 33 && registerFile.SP > 1); //SP is not -1 or some other overflow, and stack is not empty
-                this->registerFile.PC = this->memory->memory[STACK_START + this->registerFile.SP];
+                assert(registerFile.SP < 35 && registerFile.SP > 1); //SP is not -1 or some other overflow, and stack is not empty
                 this->registerFile.SP -= 2; //since stack entries are 2 bytes long
+                this->registerFile.PC = this->memory->memory[STACK_START + this->registerFile.SP + 1];
+                this->registerFile.PC += ((uint16_t)this->memory->memory[STACK_START + this->registerFile.SP]) << 8;
+                this->registerFile.PC += 2;
                 return;
             }
-            std::cerr << "Unknown instruction: " << std::hex << this->instr[this->registerFile.PC] << this->instr[this->registerFile.PC + 1] << std::dec << std::endl;
+            std::cerr << "Unknown instruction: " << std::hex << this->instr->code[this->registerFile.PC] << this->instr->code[this->registerFile.PC + 1] << std::dec << std::endl;
         }
     }
 
     void handle1instr() {
-        uint16_t higher = this->instr[this->registerFile.PC] & HIGHER_MASK;
-        uint16_t lower = this->instr[this->registerFile.PC + 1];
+        //JP addr
+        uint16_t higher = this->instr->code[this->registerFile.PC] & HIGHER_MASK;
+        uint16_t lower = this->instr->code[this->registerFile.PC + 1];
         uint16_t newPC = lower + (higher << 8);
         this->registerFile.PC = newPC;
     }
 
     void handle2instr() {
-        static_assert(this->registerFile.SP < 31);
-        this->registerFile.SP += 2;
+        //CALL addr
+        assert(this->registerFile.SP < 33);
         this->memory->memory[STACK_START + registerFile.SP] = getHigher(this->registerFile.PC);
-        this->memory->memory[STACK_START + registerFile.SP + 1] = getLower(this->registerFile.PC);
+        this->memory->memory[STACK_START + registerFile.SP + 1] = getLower(this->registerFile.PC); //TODO check this for endianness
 
-        uint16_t higher = this->instr[this->registerFile.PC] & HIGHER_MASK;
-        uint16_t lower = this->instr[this->registerFile.PC + 1];
+        uint16_t higher = this->instr->code[this->registerFile.PC] & HIGHER_MASK;
+        uint16_t lower = this->instr->code[this->registerFile.PC + 1];
         uint16_t newPC = lower + (higher << 8);
         this->registerFile.PC = newPC;
+
+        //std::cout << this->registerFile.PC << std::endl;
+
+        this->registerFile.SP += 2; //write first, then increment
     }
 
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,24 +2,36 @@
 
 #include "Memory.cpp"
 #include "CodeLoader.cpp"
+#include "../include/Processor.h"
 #include "../include/RegisterFile.h"
 #include "../include/Display.h"
 
 
+static void runCode(program* prog) {
+    auto* mem = new Memory();
+    auto* display = new Display64x32();
+
+    auto* processor = new Processor(display, mem, prog);
+    processor->runProgam();
+
+    processor->dumpRegisters();
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    auto* prog = CodeLoader::loadProgram("../res/15 Puzzle [Roger Ivie].ch8");
+    //auto* prog = CodeLoader::loadProgram("../res/15 Puzzle [Roger Ivie].ch8");
 
     //CodeLoader::dumpCode(prog);
 
     /*auto d = new Display64x32();
     d->printToConsole();*/
 
-    auto* mem = new Memory();
+    /*auto* mem = new Memory();
     mem->initDigits();
-    mem->dumpMemory("../out/mem_dump");
+    mem->dumpMemory("../out/mem_dump");*/
+
+    runCode(CodeLoader::loadProgram("../res/calltest.ch8"));
 
     return 0;
 }
